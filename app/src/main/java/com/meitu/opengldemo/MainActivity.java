3be5c13c5e268bc908a15ed92083d1f410d2d05f
zengbiyu <zby@meitu.com>
2016-01-07 19:33:47 +0800

    【Feature】在纹理上添加点

--- app/src/main/java/com/meitu/opengldemo/MainActivity.java
+++ app/src/main/java/com/meitu/opengldemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.ActivityManager;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 /**
@@ -13,6 +15,8 @@ import android.widget.Toast;
 public class MainActivity extends Activity{
 
     private GLSurfaceView glSurfaceView;
+
+    private ScrawlRender mScrawlRender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +26,30 @@ public class MainActivity extends Activity{
         boolean suportsES2 = configurationInfo.reqGlEsVersion >= 0x2000;
         if (suportsES2){
             glSurfaceView.setEGLContextClientVersion(2);
-            glSurfaceView.setRenderer(new TextureRender(this));
+            mScrawlRender = new ScrawlRender(this);
+            glSurfaceView.setRenderer(mScrawlRender);
         }else{
             Toast.makeText(this,"This device does not support ES 2.0",Toast.LENGTH_LONG).show();
             return;
         }
 
         setContentView(glSurfaceView);
+
+        glSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float normalizedX = event.getX() / (float) v.getWidth() * 2 - 1;
+                float normalizedY = -(event.getY() / (float)v.getHeight() * 2 -1);
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        mScrawlRender.handTouchDown(normalizedX,normalizedY);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return false;
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/meitu/opengldemo/objects/TextureBg.java
@@ -0,0 +1,118 @@
+package com.meitu.opengldemo.objects;
+
+import android.content.Context;
+
+import com.meitu.opengldemo.R;
+import com.meitu.opengldemo.utils.ShaderHelper;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_TEXTURE0;
+import static android.opengl.GLES20.GL_TEXTURE_2D;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.glActiveTexture;
+import static android.opengl.GLES20.glBindTexture;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform1i;
+import static android.opengl.GLES20.glUniformMatrix4fv;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+
+/**
+ * Created by zby on 2016/1/7.
+ */
+public class TextureBg {
+
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int TEXTURE_COMPONENT_COUNT = 2;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + TEXTURE_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
+    // Uniform constants
+    protected static final String U_MATRIX = "u_Matrix";
+    protected static final String U_TEXTURE_UNIT = "u_TextureUnit";
+
+    // Attribute constants
+    protected static final String A_POSITION = "a_Position";
+    protected static final String A_TEXTURE_COORDINATES = "a_TextureCoordinates";
+
+    private int aPositionLocation;
+    private int aTextureCoordinatesLocation;
+
+    private int uMatrixLocation;
+    private int uTextureUnitLocation;
+
+    private FloatBuffer vertexData;
+
+    private Context context;
+
+    private int program;
+
+    public TextureBg(Context context) {
+        this.context = context;
+        float[] tableVerticesWithTriangles = {
+                // Order of coordinates: X, Y, ,S,T
+
+                // Triangle Fan
+                0f, 0f, 0.5f, 0.5f,
+                -1f, -1f, 0f, 1f,
+                1f, -1f, 1f, 1f,
+                1f, 1f, 1f, 0f,
+                -1f, 1f, 0f, 0f,
+                -1f, -1f, 0f, 1f,
+
+
+        };
+        vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+        program = ShaderHelper.createProgram(context, R.raw.texture_vertex_shader, R.raw.texture_fragment_shader);
+
+        uTextureUnitLocation = glGetUniformLocation(program, U_TEXTURE_UNIT);
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        aTextureCoordinatesLocation = glGetAttribLocation(program, A_TEXTURE_COORDINATES);
+    }
+
+    public void useProgram(){
+        glUseProgram(program);
+    }
+
+    public void  bindData(float[] projectionMatrix,int textureId){
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aTextureCoordinatesLocation, TEXTURE_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
+
+        glEnableVertexAttribArray(aPositionLocation);
+        glEnableVertexAttribArray(aTextureCoordinatesLocation);
+
+        // Pass the matrix into the shader program.
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
+
+        // Set the active texture unit to texture unit 0.
+        glActiveTexture(GL_TEXTURE0);
+
+        // Bind the texture to this unit.
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
+        // Tell the texture uniform sampler to use this texture in the shader by
+        // telling it to read from texture unit 0.
+        glUniform1i(uTextureUnitLocation, 0);
+
+    }
+
+    public void drawSelf(){
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+    }
+
+
+}
